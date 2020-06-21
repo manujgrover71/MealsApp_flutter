c9ce0373b5b081a8c20887c801a353008cbf6fe0
Manuj Grover <manujgrover71@gmail.com>
2020-06-21 20:26:21 +0530

    added fonts and navigation using push method

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -12,7 +12,7 @@ class CategoriesScreen extends StatelessWidget {
       ),
       body: GridView(
         children: DUMMY_CATEGORIES.map((item) {
-          return CategoryItem(item.title, item.color);
+          return CategoryItem(item.id, item.title, item.color);
         }).toList(),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'categories_screen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -10,9 +11,24 @@ class MyApp extends StatelessWidget {
       title: 'DeliMeals',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        accentColor: Colors.yellow[500],
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              body2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              title: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              )
+            ),
       ),
       home: CategoriesScreen(),
     );
   }
 }
-
